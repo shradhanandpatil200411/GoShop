b0398bdd0d886c3d6e29165abb75fdb388365293
shradhanand patil <user@example.com>
2025-11-27 23:28:20 +0530

    feat: add Google GenAI integration and socket.io dependency
    - Added @google/genai package to enable AI content generation.
    - Introduced Ai.service.js for handling AI response generation using the Gemini model.
    - Included socket.io for real-time communication capabilities in the backend.

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -1,7 +1,26 @@
 const app = require("./Src/app");
 const connectDB = require("./Src/DB/DB");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const generateResponse = require("./Src/Service/Ai.service");
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
+
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => {
+    console.log("Disconnect");
+  });
+
+  socket.on("ai-message", async (data) => {
+    const response = await generateResponse(data.prompt);
+    socket.emit("ai-response", { response });
+  });
+});
 
 connectDB();
-app.listen(process.env.PORT, () => {
+httpServer.listen(process.env.PORT, () => {
   console.log(`server is start on ${process.env.PORT} port`);
 });
